e261bc45426eb4c15712a68f3120766340cb703e
aniketsalgaonkar <aniketsalgaonkar.nit@gmail.com>
2024-07-19 18:44:02 +0530

    initial commit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div className="footer">
+      <p>Copyright @ 2024, CryptoGauge </p>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/context/CoinContext.jsx
@@ -0,0 +1,42 @@
+import { createContext, useState, useContext, useEffect } from "react";
+
+const CoinContext = createContext();
+
+const CoinContextProvider = (props) => {
+  const [allCoin, setAllCoin] = useState([]);
+  const [currency, setCurrency] = useState({
+    name: "usd",
+    symbol: "$",
+  });
+
+  const fetchAllCoin = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": "CG-vsJ7H49xVoaRAKocBBsu1vku",
+      },
+    };
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`,
+      options
+    )
+      .then((response) => response.json())
+      .then((response) => setAllCoin(response))
+      .catch((err) => console.error(err));
+  };
+
+  useEffect(() => {
+    fetchAllCoin();
+  }, [currency]);
+
+  const contextValue = { allCoin, currency, setCurrency };
+
+  return (
+    <CoinContext.Provider value={contextValue}>
+      {props.children}
+    </CoinContext.Provider>
+  );
+};
+export { CoinContext };
+export default CoinContextProvider;
